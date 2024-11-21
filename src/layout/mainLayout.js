0f089137cf55d6a37745ee97662ab06a7d45789d
Alexisgfrias <majestickrono@outlook.com>
2024-11-21 10:51:40 -0600

    Vista responsiva del sidebar para movil

--- src/layout/mainLayout.js
+++ src/layout/mainLayout.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import Sidebar from "../components/Sidebar";
+import { useMediaQuery } from "@mui/material";
+import MobileNavigation from "../components/Navigationbar";
 import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   const [open, setOpen] = useState(false);
+  const isMobile = useMediaQuery("(max-width:600px)");
 
   // Función para alternar el Drawer
   const toggleDrawer = () => {
@@ -12,7 +15,11 @@ const MainLayout = () => {
 
   return (
     <div style={{ display: "flex" }}>
-      <Sidebar open={open} toggleDrawer={toggleDrawer} />
+      {isMobile ? (
+        <MobileNavigation />
+      ) : (
+        <Sidebar open={open} toggleDrawer={toggleDrawer} />
+      )}
       <div
         style={{
           marginLeft: open ? 10 : 40,
